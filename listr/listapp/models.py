7524f82a4ae5380fe6b1b961c489fff49ba8fac7
Titus Jung <titusjung@gmail.com>
2016-09-12 16:25:08 -0700

    start on homepage

--- listr/listapp/models.py
+++ listr/listapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model):
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey('auth.User')
+    description = models.TextField()
+    created_at = models.DateTimeField()
+    modified_at = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "entries"
